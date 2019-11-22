871ae9c90a5a0513b5af936010710994a18eef71
Yuri Kimura <yuri.kimura@luxiar.com>
2019-11-22 09:54:35 +0900

    Close #3 enumのメソッドに変更、messageメソッド作成

--- app/controllers/attendance_managements_controller.rb
+++ app/controllers/attendance_managements_controller.rb
@@ -6,18 +6,22 @@ class AttendanceManagementsController < ApplicationController
   def create
     @attendance_management = AttendanceManagement.new(attendance_params)
     if @attendance_management.save
-      if @attendance_management.attendance == 'going'
-        redirect_to new_attendance_management_path,
-        notice: "#{ @attendance_management.employee.name }さん、おはようございます。"
-      else
-        redirect_to new_attendance_management_path,
-        notice: "#{ @attendance_management.employee.name }さん、お疲れ様です。"
-      end
+      redirect_to new_attendance_management_path, notice: message
+    else
+      render 'new'
     end
   end
 
   private
   def attendance_params
     params.require(:attendance_management).permit(:attendance, :time, :employee_id)
+  end
+
+  def message
+    if @attendance_management.going?
+      "#{ @attendance_management.employee.name }さん、おはようございます。"
+    else
+      "#{ @attendance_management.employee.name }さん、お疲れ様です。"
+    end
   end
 end

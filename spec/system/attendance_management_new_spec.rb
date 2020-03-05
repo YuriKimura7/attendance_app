06e4e1a4918b799bff261bdbf482ed175cfefc1f
Yuri Kimura <yuri.kimura@luxiar.com>
2020-03-05 10:15:22 +0900

    レビュー修正、必要最低限のオブジェクトの属性を検証

--- spec/system/attendance_management_new_spec.rb
+++ spec/system/attendance_management_new_spec.rb
@@ -5,15 +5,6 @@ RSpec.describe 'attendance_management_new', type: :system do
     Employee.create(name: 'おもす', number: 1)
     Employee.create(name: '木村', number: 2)
 
-    expected_value = {
-      id: 1,
-      attendance: "going",
-      time: DateTime.new(2020,3,4,10,00),
-      employee_id: 2,
-      created_at: Time.now,
-      updated_at: Time.now
-    }
-
     visit '/attendance_managements/new'
     select('木村', from: 'attendance_management_employee_id')
     select('2020', from: 'attendance_management[time(1i)]')
@@ -23,16 +14,10 @@ RSpec.describe 'attendance_management_new', type: :system do
     select('00', from: 'attendance_management[time(5i)]')
     select('出社', from: 'attendance_management_attendance')
     click_button('登録')
-    kimura = {
-      id: AttendanceManagement.last.id,
-      attendance: AttendanceManagement.last.attendance,
-      time: AttendanceManagement.last.time,
-      employee_id: AttendanceManagement.last.employee_id,
-      created_at: AttendanceManagement.last.created_at,
-      updated_at: AttendanceManagement.last.updated_at
-    }
-    binding.pry
-    expect(kimura).to eq(expected_value)
-    # expect(page).to have_content '木村さん、おはようございます。'
+
+    kimura = AttendanceManagement.last
+
+    expect(kimura).to have_attributes(attendance: 'going', time: DateTime.new(2020,3,4,10,00))
+    expect(page).to have_content '木村さん、おはようございます。'
   end
 end
